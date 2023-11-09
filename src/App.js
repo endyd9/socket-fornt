138ad4badcf61fb9fcf7006ce2974d9654feee85
dooyongkim <dooyong@blocksmith.xyz>
2023-11-09 13:41:15 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client"
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [token, setToken] = useState("")
+    const [id, setId] = useState()
+
+    const connectSocket = () => {
+        const ws = io.connect('http://localhost:4000', {
+            auth: {
+                token
+            }
+        })
+        ws.on('event', () => {
+            console.log("ㅎㅇ")
+        })
+        ws.on("message", (msg) => {
+            console.log(msg)
+        })
+        ws.on('event2', () => {
+            console.log('ㅂㅇ')
+        })
+    }
+
+    const getToken = async () => {
+        await fetch(`http://localhost:4000/${id}`).then(async (res) => {
+            const {token} = await res.json()
+            setToken(token)
+        }).finally(() => connectSocket())
+
+    }
+
+
+
+    return (
+        <div className="App">
+            <h1>sockettest</h1>
+            <button onClick={async () => {
+                await fetch("http://localhost:4000/alert/a")
+            }}>event
+            </button>
+            <button onClick={async () => {
+                await fetch("http://localhost:4000/alert/b")
+            }}>event2
+            </button>
+            <input type="text"  value={id} onChange={(e) => setId(() => e.target.value)}/>
+            <button onClick={getToken}>getToken</button>
+        </div>
+    );
 }
 
 export default App;
